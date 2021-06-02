e52cd8e93319226cc882c8a967475457c530b96f
yorifuji <yorifuji@gmail.com>
2021-06-02 09:28:21 +0900

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import feedparser # pip install feedparser
 import datetime
 import time
+import sys
 
 class Article:
   def __init__(self, json):
@@ -9,10 +10,15 @@ class Article:
     self.link       = json['link']
     self.published  = datetime.datetime.fromtimestamp(time.mktime(json['published_parsed'])+3600*9).isoformat() + "+09:00"
 
-username='yorifuji'
+def parse(username):
+  articles_json = feedparser.parse('https://zenn.dev/' + username + '/feed').entries
+  articles = sorted([Article(json) for json in articles_json], key=lambda article: article.published)
 
-articles_json = feedparser.parse('https://zenn.dev/' + username + '/feed').entries
-articles = sorted([Article(json) for json in articles_json], key=lambda article: article.published)
+  for article in articles:
+    print(article.published, article.link, article.title, sep=',')
 
-for article in articles:
-  print(article.published, article.link, article.title, sep=',')
+if __name__=='__main__':
+  if len(sys.argv) > 0:
+    parse(sys.argv[1])
+  else:
+    print("Usage: main.py [username]")
